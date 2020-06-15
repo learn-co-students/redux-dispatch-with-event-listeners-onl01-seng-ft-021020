3f9d1ab27bfee13b6cb6d3b0b8ebd5499c5831e3
reddevilcero <reddevil_cero@hotmail.com>
2020-06-15 13:00:28 +0100

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,31 @@
 // add code snippets from README
+
+let state;
+
+document
+  .getElementById("button")
+  .addEventListener("click", function increase() {
+    dispatch({ type: "INCREASE_COUNT" });
+  });
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return {
+        count: state.count + 1,
+      };
+
+    default:
+      return state;
+  }
+}
+function render() {
+  document.getElementById("container").innerText = state.count;
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+dispatch({ type: "@@INIT" });
